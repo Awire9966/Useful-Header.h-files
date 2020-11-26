2aab0bd4220df9d2f58e04dc9d277cd1c6fefc03
Awire9966 <65136929+Awire9966@users.noreply.github.com>
2020-11-25 19:13:32 -0500

    Create BestCommandLinker.h

---
+++ BestCommandLinker.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <codecvt>
+#include <unordered_map>
+#include <chrono>
+#include <TlHelp32.h>
+#include <filesystem>
+#include <fstream>
+#include <Windows.h>
+#include <cstdio>
+#include <Windows.h>
+#include <Psapi.h>
+#include <shellapi.h>
+#include <Windows.h>
+#include <WinInet.h>
+#include <string>
+#include <regex>
+#include <stdlib.h>
+
